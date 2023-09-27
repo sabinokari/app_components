3dd743c4a7a038f365273b8e91c39ee3a579d362
SABINO KARI B <202100977b@utea.edu.pe>
2023-09-27 06:41:02 -0500

    app_components

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
